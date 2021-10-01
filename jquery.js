6e01e7f1ba08e37c49fb30a121f3bd61dedc3bb7
Nicolas <nicolagardo@gmail.com>
2021-10-01 16:14:35 -0300

    clickAction

--- jquery.js
+++ jquery.js
@@ -1,18 +1,25 @@
 
-  $(document).ready(function(){
-
-    
-
-
-});
+ 
 $(function(){
    /* $("p").css({"background-color":"red"});
     $("h1").css({"background-color": "red"}); */
     $("#arrastrable").draggable();
     $( "#acordeon" ).accordion();
+    $( "#acordeon" ).mousedown(function() {
+        console.log('clicked!')
+      });
 
 
+    $( "h1" ).mousedown(function() {
+        console.log('clicked!')
+      });
+      
+      $( "#clickAction" ).mousedown(function() {
+        if($(this).hasClass('blue')){
+            $(this).removeClass('blue');
+        }else{
+          $(this).addClass('blue');
+        }
+
+    });
 });
-
-  $( function() {
-  } );
