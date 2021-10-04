458a513b2dfe7e343ae80f27e75ac7639526f1d4
Nicolas <nicolagardo@gmail.com>
2021-10-04 20:33:36 -0300

    parrafo mas grande

--- jquery.js
+++ jquery.js
@@ -4,21 +4,23 @@ $(function(){
    /* $("p").css({"background-color":"red"});
     $("h1").css({"background-color": "red"}); */
     $("#arrastrable").draggable();
+
     $( "#acordeon" ).accordion();
-    $( "#acordeon" ).mousedown(function() {
+
+    $("#arrastrable").mousedown(function() {
         console.log('clicked!')
-      });
+    });
 
 
-    $( "h1" ).mousedown(function() {
+    $("#clickAction").mousedown(function() {
         console.log('clicked!')
-      });
+    });
       
-      $( "#clickAction" ).mousedown(function() {
+    $( "#clickAction" ).mousedown(function() {
         if($(this).hasClass('blue')){
             $(this).removeClass('blue');
         }else{
-          $(this).addClass('blue');
+            $(this).addClass('blue');
         }
 
     });
